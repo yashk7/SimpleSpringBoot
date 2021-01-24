ee43fdc895f01fbccd9e990ae514b4782367213b
yashk7 <yashkosalge@gmail.com>
2021-01-24 14:24:56 +0530

    Initial commit

---
+++ src/main/java/com/yash/demo/Controller/SimpleController.java
@@ -0,0 +1,26 @@
+package com.yash.demo.Controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SimpleController {
+	
+	@GetMapping({"/","/hello"})
+	public String hello(@RequestParam(value ="name",defaultValue="World",required =true) String name,Model model)
+	{
+		System.out.println("The data is been invoked");
+		String data="HEXAWARE";
+		
+		model.addAttribute("name",data);
+		
+		
+		return "hello";
+	}
+	
+
+}
